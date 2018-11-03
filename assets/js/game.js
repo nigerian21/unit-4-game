d7704158c318e1dc24a59aea4c8acfdce1aaf52c
Efe <efeowolide@gmail.com>
2018-11-03 10:31:30 -0500

    trying to make game more functional

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,19 +14,43 @@ var m = 55;
 1
 $(".wins").text(won);
 $(".loss").text(lost);
- $(".number").append(number);
+$(".number").append(number);
 $("#inner").append("<p>Test</p>");
 //if the guessed number and the numbers added up by the crystals equal each other then an alert will pop up saying you won and refresh the page 
 if (yournumber === number) {
     alert("you won get ready for round 2")
     window.location.reload()
 };
-$(".c1").on("click", function( ) { 
+$(".c1").on("click", function () {
     d++;
-    $(".yournum").text(d);
+    $(".yournum").html(d);
 
 
 });
+
+$(".c2").on("click", function () {
+    i++;
+    $(".yournum").html(i);
+
+
+});
+
+$(".c3").on("click", function () {
+    a++;
+    $(".yournum").html(a);
+
+
+});
+
+$(".c4").on("click", function () {
+    m++;
+    $(".yournum").html(m);
+
+
+});
+
+
+
 
 
 
